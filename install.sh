5fd0c34b67ef53a5c53febeb796b122210314a99
Dmitry Scherbakov <git@sherbacov.com>
2026-03-18 20:43:11 +0300

    Add dnsmasq-full to install commands (required for nftset support)

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ set -e
 
 echo "=== PassWall2 Installer ==="
 
-echo "[1/4] Adding signing key..."
+echo "[1/6] Adding signing key..."
 wget -O /etc/apk/keys/passwall2-repo.rsa.pub \
   https://rage-ac.github.io/Passwall2/keys/passwall2-repo.rsa.pub
 
-echo "[2/4] Adding repository..."
+echo "[2/6] Adding repository..."
 grep -q 'rage-ac.github.io/Passwall2' /etc/apk/repositories || \
   echo "https://rage-ac.github.io/Passwall2/packages" >> /etc/apk/repositories
 
-echo "[3/4] Updating package index..."
+echo "[3/6] Updating package index..."
 apk update
 
-echo "[4/5] Installing PassWall2..."
+echo "[4/6] Installing PassWall2..."
 apk add luci-app-passwall2
 
-echo "[5/5] Installing nftables dependencies..."
-apk add kmod-nft-socket kmod-nft-tproxy
+echo "[5/6] Installing dependencies..."
+apk add kmod-nft-socket kmod-nft-tproxy dnsmasq-full
 
 echo "[6/6] Restarting PassWall2..."
 /etc/init.d/passwall2 enable
